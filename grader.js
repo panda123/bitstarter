19ebd4494e6a18191e02abaf7f187f8d5b4408a2
Ubuntu <ubuntu@ip-172-31-42-153.us-west-2.compute.internal>
2013-07-20 23:31:56 +0000

    updated grader.js to work for url

--- grader.js
+++ grader.js
@@ -24,14 +24,17 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://mysterious-cove-9198.herokuapp.com"
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+        process.exit(1);
+ // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
@@ -63,12 +66,31 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-c, --checks <check_file>', 'Path to checks.json')
+        .option('-f, --file <html_file>', 'Path to index.html')
+        .option('-u, --url <url_file>', 'Url to html file')
         .parse(process.argv);
+
+ if(program.url) 
+{
+rest.get(program.url).on('complete', function(result) {
+  if (result instanceof Error) {
+    sys.puts('Error: ' + result.message);
+    this.retry(5000);
+  } else {
+ fs.writeFileSync('outfile.html', result);
+                var checkJson = checkHtmlFile('outfile.html', program.checks);
+                var outJson = JSON.stringify(checkJson, null, 4);
+                console.log(outJson);
+  }
+});
+                        
+}
+ if(program.file){
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+}
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
